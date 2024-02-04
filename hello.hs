986e1e63e8693b3c2c4d01fe609ddc935334e18b
thousanda <k0k1senda@gmail.com>
2024-02-04 22:36:05 +0900

    case of

--- hello.hs
+++ hello.hs
@@ -1,8 +1,8 @@
-fib n
-  | n == 1 = 0
-  | n == 2 = 1
-  | n > 0  = fib (n-2) + fib (n-1)
+fib n = case n of
+  1 -> 0
+  2 -> 1
+  _ | n > 0 -> fib (n-2) + fib (n-1)
 
 main :: IO ()
 main = do
-  mapM_ (\i -> print (fib i)) [0..10]
+  mapM_ (\i -> print (fib i)) [1..10]
