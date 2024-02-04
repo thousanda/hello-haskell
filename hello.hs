ab2bc0a90ca5b132fb5ec12a5b4e62e62dd5243c
thousanda <k0k1senda@gmail.com>
2024-02-04 22:33:52 +0900

    error with negative value

--- hello.hs
+++ hello.hs
@@ -1,9 +1,8 @@
 fib n
-  | n == 1    = 0
-  | n == 2    = 1
-  | n <= 0    = 0
-  | otherwise = fib (n-2) + fib (n-1)
+  | n == 1 = 0
+  | n == 2 = 1
+  | n > 0  = fib (n-2) + fib (n-1)
 
 main :: IO ()
 main = do
-  mapM_ (\i -> print (fib i)) [1..10]
+  mapM_ (\i -> print (fib i)) [0..10]
