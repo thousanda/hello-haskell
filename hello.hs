b1291d938b3ca9018db7cbdd5cec9ad8073ce410
thousanda <k0k1senda@gmail.com>
2024-02-11 21:18:36 +0900

    sum, product, take, drop, reverse

--- hello.hs
+++ hello.hs
@@ -1,8 +1,24 @@
-fib n = case n of
-  1 -> 0
-  2 -> 1
-  _ | n > 0 -> fib (n-2) + fib (n-1)
+sum' [] = 0
+sum' (x:xs) = x + (sum' xs)
+
+product' [] = 1
+product' (x:xs) = x * (product' xs)
+
+take' _ [] = []
+take' n _ | n < 1 = []
+take' n (x:xs) = x : take' (n-1) xs
+
+drop' _ [] = []
+drop' n xs | n < 1 = xs
+drop' n (_:xs) = drop' (n-1) xs
+
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
 
 main :: IO ()
 main = do
-  mapM_ (\i -> print (fib i)) [1..10]
+  print $ sum' [1..10]
+  print $ product' [1..5]
+  print $ take' 3 [1..10]
+  print $ drop' 3 [1..10]
+  print $ reverse' [1..10]
